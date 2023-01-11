b9f74b1e934af9af1a7a736b44ad52158dedace8
DevMovegun <developer_movegun@naver.com>
2023-01-12 01:45:51 +0900

    AOP를 사용하지 않고 일일히 시간측정 로직을 적용

--- helloSpring/helloSpring/src/main/java/studySpring/helloSpring/service/MemberService.java
+++ helloSpring/helloSpring/src/main/java/studySpring/helloSpring/service/MemberService.java
@@ -19,12 +19,25 @@ public class MemberService { // class 내에서 Ctrl+Shift+T를 누르면 자동
     public MemberService(MemberRepository memberRepository) { // class 내에서 new 객체를 생성하지 않고 외부에서 받음 => Dependency Injection
         this.memberRepository = memberRepository;
     }
+
     // 회원 가입
     public Long join(Member member) {
 
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        // 직접 했을 때
+        // 공통 관심사항(cross-cutting concern)인 시간을 측정하는 로직을 위해 코드가 복잡해져서
+        // 핵심 관심사항(core concern)인 join service 로직에 유지보수가 어려워짐
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
     }
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
@@ -35,7 +48,17 @@ public class MemberService { // class 내에서 Ctrl+Shift+T를 누르면 자동
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
     }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
